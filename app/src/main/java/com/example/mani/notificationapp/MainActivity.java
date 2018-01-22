3b07a74147220bf42d424b67fd5f97ace3011300
Mani Kumar <mk0872191152@gmail.com>
2018-01-22 21:10:07 +0530

    Add register functionality to allow user signup

--- app/src/main/java/com/example/mani/notificationapp/MainActivity.java
+++ app/src/main/java/com/example/mani/notificationapp/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.mani.notificationapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button mLogoutButton;
     private FirebaseAuth mAuth;
 
     @Override
@@ -34,5 +37,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+        mLogoutButton = (Button)findViewById(R.id.btn_logout);
+
+        mLogoutButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        mAuth.signOut();
+        Intent logoutIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(logoutIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/mani/notificationapp/RegisterActivity.java
+++ app/src/main/java/com/example/mani/notificationapp/RegisterActivity.java
@@ -1,28 +1,46 @@
 package com.example.mani.notificationapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mName, mEmail, mPassword;
-    FirebaseAuth mAuth;
+    private ProgressBar mProgressBar;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
+        mStore = FirebaseFirestore.getInstance();
+
         mName = (EditText) findViewById(R.id.et_user_name);
         mEmail = (EditText) findViewById(R.id.et_user_email_regitser);
         mPassword = (EditText) findViewById(R.id.user_password_regitser);
+        mProgressBar = (ProgressBar) findViewById(R.id.pb_register);
         Button mRegisterButton = (Button) findViewById(R.id.btn_register);
         Button mAlreadyAccount = (Button) findViewById(R.id.btn_account_already);
 
@@ -37,13 +55,44 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 finish();
                 break;
             case R.id.btn_register:
-                String name = mName.getText().toString();
-                String email = mEmail.getText().toString();
+                mProgressBar.setVisibility(View.VISIBLE);
+                final String name = mName.getText().toString();
+                final String email = mEmail.getText().toString();
                 String password = mPassword.getText().toString();
 
                 if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                String user_id = mAuth.getCurrentUser().getUid();
 
+                                Map<String, String> userMap = new HashMap<>();
+                                userMap.put("name", name);
+                                userMap.put("email", email);
+
+                                mStore.collection("Users").document(user_id).set(userMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        mProgressBar.setVisibility(View.INVISIBLE);
+                                        sendToMain();
+                                    }
+                                });
+                            } else {
+                                mProgressBar.setVisibility(View.INVISIBLE);
+
+                                Toast.makeText(RegisterActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
                 }
+                break;
         }
+    }
+
+    private void sendToMain() {
+        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
     }
 }
