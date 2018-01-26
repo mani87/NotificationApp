c3dc2eb5e8fde1fa730637b05e505e4ed17fd104
Mani Kumar <mk0872191152@gmail.com>
2018-01-26 19:53:44 +0530

    Add more functionality to app

--- app/src/main/java/com/example/mani/notificationapp/MainActivity.java
+++ app/src/main/java/com/example/mani/notificationapp/MainActivity.java
@@ -5,13 +5,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mLogoutButton;
+    private TextView mProfileName;
+    private FirebaseFirestore mFirestore;
+    private String mUserId;
     private FirebaseAuth mAuth;
 
     @Override
@@ -37,7 +44,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-        mLogoutButton = (Button)findViewById(R.id.btn_logout);
+        mFirestore = FirebaseFirestore.getInstance();
+
+        mUserId = mAuth.getCurrentUser().getUid();
+        mLogoutButton = (Button) findViewById(R.id.btn_logout);
+        mProfileName = (TextView) findViewById(R.id.tv_profile_name);
+
+        mFirestore.collection("Users").document(mUserId).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                String user_name = documentSnapshot.getString("name");
+                mProfileName.setText(user_name);
+            }
+        });
 
         mLogoutButton.setOnClickListener(this);
     }
