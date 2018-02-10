921229f58cbfc92d26b56485760c48d25bebb8e8
Mani Kumar <mk0872191152@gmail.com>
2018-02-10 12:44:04 +0530

    Add song when notification come

--- app/src/main/java/com/example/mani/notificationapp/Firebase/FirebaseMessagingService.java
+++ app/src/main/java/com/example/mani/notificationapp/Firebase/FirebaseMessagingService.java
@@ -2,6 +2,9 @@ package com.example.mani.notificationapp.Firebase;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.example.mani.notificationapp.R;
@@ -17,6 +20,8 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
+        Context context = this;
+
         String messageTitle = remoteMessage.getNotification().getTitle();
         String messageBody = remoteMessage.getNotification().getBody();
 
@@ -25,8 +30,9 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setContentTitle(messageTitle)
                         .setContentText(messageBody)
-                        .setDefaults(Notification.DEFAULT_ALL)
-                        .setPriority(Notification.PRIORITY_HIGH);
+                        .setVibrate(new long[] {1000, 1000, 1000, 1000, 1000, 1000, 1000 ,1000})
+                        .setSound(Uri.parse("android.resource://com.example.mani.notificationapp/raw/song"))
+                        .setPriority(NotificationCompat.PRIORITY_HIGH);
 
         int mNotificationId = (int) System.currentTimeMillis();
 
